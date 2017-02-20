dca1135df993ddd12ebb00c63b2c7ca44e45befc
Eddy Salazar <e-salazar@outlook.com>
2017-02-20 10:29:04 -0600

    Initial commit

---
+++ src/testEclisePackage/TestEclipse.java
@@ -0,0 +1,12 @@
+package testEclisePackage;
+
+public class TestEclipse {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World"); 
+		
+		//This is the first change
+		System.out.println("Hello");
+	}
+
+}
